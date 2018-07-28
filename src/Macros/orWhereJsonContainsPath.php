8281534f2e003e88e87a2c348f8fb9ed2304da25
Aidyn Makhataev <makataev.7@gmail.com>
2018-07-28 22:22:36 +0600

    Add whereJsonContainsPath Macros

---
+++ src/Macros/orWhereJsonContainsPath.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Eloquent\Builder;
+use AidynMakhataev\EloquentJsonMacros\EloquentJsonMacros;
+
+
+/**
+ * Add a orWhere "json_contains_path" clause to the query.
+ * 
+ * @param string $column
+ * @param mixed $path
+ * @param string $oneOrAll
+ * 
+ * @return Builder
+ */
+Builder::macro('orWhereJsonContainsPath', function ($column, $path, $oneOrAll = 'one') {
+    
+    if(is_array($path)) {
+        foreach($path as $key => $item) {
+            $path[$key] = "'" . EloquentJsonMacros::formatJsonPath($item) . "'";
+        }
+        $path = implode(',', $path);
+    } else {
+        $path = "'" . EloquentJsonMacros::formatJsonPath($path) . "'";    
+    }
+
+    return $this->orWhereRaw("JSON_CONTAINS_PATH($column, '$oneOrAll', $path)");
+});
